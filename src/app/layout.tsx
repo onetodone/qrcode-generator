166c5a646a5b040dde7ed5de67f3cbbb3927ede9
Anton Holubeu <131243822+aholu@users.noreply.github.com>
2026-09-12 19:32:47 +0400

    feat: add-Vercel Speed Insights (#17)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Toaster } from '@/components/ui/sonner'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 const geistSans = Geist({
@@ -42,6 +43,7 @@ export default function RootLayout({ children }: LayoutProps<'/'>) {
       <body className="min-h-full flex flex-col">
         {children}
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   )
